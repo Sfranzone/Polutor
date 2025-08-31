296d0cfdf66e05953f109febebd7d71b54533f9b
Sébastien Franzone <s.franzone@hotmail.fr>
2025-08-31 22:20:27 +0200

    Ajustements descriptions cartes

--- scripts2/CardDatabase.gd
+++ scripts2/CardDatabase.gd
@@ -1,12 +1,12 @@
 const CARDS = { # mana, cout social, cout env., dommage, description de la carte]
-	"Eolapino" : [5, 6, 2, 10, "Avec ses amis éoliennes, Eolapino produit de l'électricité grâce au vent"],
-	"Nuclearo" : [5, 2, 6, 20, "La centrale et ses enfants ne prennent pas beaucoup de place, mais polluent l'atmosphère"],
-	"Barrachat" : [4, 6, 2, 15, "Un barrage bien contrôler permet de produire beaucoup de manière naturel en créant même de nouveau écosystème!"],
-	"Thermhibou" : [2, 1, 4, 15, "Une centrale thermique"],
-	"Panolero" : [5, 5, 5, 12, "Un champs de panneaux solaires"],
-	"Amigo Tri" : [4, 5, 0, 1, "Tout le monde doit trier ses déchets !"],
-	"Nuclearo Destructo" : [3, 9, 0, 3, "Destruction d'un Nuclearo"],
-	"Taxito Gratuito" : [2, 0, 7, 4, "Les taxis sont dorénavant gratuit et les conducteurs sont rémunérés par le gouvernement."],
-	"Climato Froido" : [3, 0, 6, 2, "Subvention du gouvernement pour des climatiseurs gratuits pour tout le monde."],
-	"Foresto Anouvo" : [0, 0, 0, 0, "Création de zone forestière en replantant des arbres à grande échelle."]
+	"Eolapino" : [5, 6, 2, 10, "Avec ses amis éoliennes, ils produisent de un peu d'électricité grâce au vent tout en réduisant la quantité de monstres polluants apparaissant sur la planète."],
+	"Nuclearo" : [5, 2, 6, 20, "Construction d'une centrale nucléaire ne prenant pas beaucoup de place pour la quantité d'énergie produite, mais qui contribue un peu à la création d'habitat pour les monstres."],
+	"Barrachat" : [4, 6, 2, 15, "Un barrage bien contrôler permet de produire beaucoup de manière naturel en créant même de nouveau écosystème !"],
+	"Thermhibou" : [2, 1, 4, 15, "Une centrale thermique qui permet de produire beaucoup d'éléctricité, mais attention, en avoir trop augmente le pouvoir des monstres !"],
+	"Panolero" : [5, 5, 5, 12, "Un champs de panneaux solaires qui produit un peu d'énergie à l'aide du soleil sans donner de moyen aux monstres de pollution de croître"],
+	"Amigo Tri" : [4, 5, 0, 1, "Instauration d'une lois pour le tri des déchets pour tous sous peine d'amende salée."],
+	"Nuclearo Destructo" : [3, 9, 0, 3, "Destruction d'un Nuclearo, cela permet de créer moins de déchets nucléaires, mais réduit la puissance éléctrique du monde"],
+	"Taxito Gratuito" : [2, 0, 7, 4, "Les taxis sont dorénavant gratuit et les conducteurs sont rémunérés par le gouvernement pour des voyages plus pratique, mais contribuant un peu à la satisfaction des monstres."],
+	"Climato Froido" : [3, 0, 6, 2, "Subvention du gouvernement pour des climatiseurs gratuits pour tout le monde qui permettent à tout le monde de vivre dans un grand confort au prix de l'environnement."],
+	"Foresto Anouvo" : [0, 0, 0, 0, "Mobilisation de la population pour la création de zones forestières en replantant des arbres à grande échelle."]
 }
