a2226d3b052e3aedb9fb49c24ee23c8edcf2f95f
Séb <s.franzone@hotmail.fr>
2025-09-18 15:23:20 +0200

    Modifying scripts

--- scripts2/BattleManagerTuto.gd
+++ scripts2/BattleManagerTuto.gd
@@ -1,1 +1,122 @@
 extends Node
+
+var monster_buff_25 = false
+var monster_buff_75 = false
+
+
+func _process(delta: float) -> void:
+	if Global.env_soc_gauge < 25:
+		monster_buff_25 = true
+		monster_buff_75 = false
+	elif Global.env_soc_gauge > 75:
+		monster_buff_75 = true
+		monster_buff_25 = false
+	else:
+		monster_buff_25 = false
+		monster_buff_75 = false
+
+
+func _ready() -> void:
+	pass
+
+
+func _on_end_turn_button_pressed() -> void:
+	monster_turn()
+
+
+func monster_turn():
+	Global.monster_turn = true
+	# Start opponent turn : disable/hide button + prevent action from player
+	$"../EndTurnButton".disabled = true
+	$"../EndTurnButton".visible = false
+	
+	# Wait opponent finish action
+	await get_tree().create_timer(1).timeout
+	
+	
+	
+	if monster_buff_25 or monster_buff_75:
+		if randf() < 0.1:
+			# Monster basic attack
+			monster_basic_attack()
+			await get_tree().create_timer(2).timeout
+		else:
+			# Monster big atack
+			monster_big_attack()
+			await get_tree().create_timer(4).timeout
+	else:
+		if randf() < 0.7:
+			# Monster basic attack
+			monster_basic_attack()
+			await get_tree().create_timer(2).timeout
+		else:
+			# Monster big atack
+			monster_big_attack()
+			await get_tree().create_timer(4).timeout
+	
+	
+	end_monster_turn()
+
+
+func monster_basic_attack():
+	$"../Enemy2/BasicAttack/BasicAttackMove".play("AttackMove")
+	await get_tree().create_timer(1).timeout
+	$"../MonsterBaseAttack".play()
+	#battle_timer1s.start()
+	#await battle_timer1s.timeout
+	
+	# HP damage
+	#$"../PlayerHealth".health_damage(5)
+	
+	# Turn reduce
+	#$"../TurnGauge".set_turn_down(1)
+	
+	# Round turn
+	await get_tree().create_timer(1).timeout
+	$"../RoundTurnGauge".set_turn_up(1)
+
+
+func monster_big_attack():
+	$"../Enemy2/BigAttack/BigAttackMove".play("BigAttack")
+	await get_tree().create_timer(2).timeout
+	$"../MonsterBigAttack".play()
+	#battle_timer3s.start()
+	#await battle_timer3s.timeout
+	
+	# HP damage
+	#await get_tree().create_timer(1).timeout
+	#$"../PlayerHealth".health_damage(15)
+	
+	# Turn reduce
+	#await get_tree().create_timer(2.5).timeout
+	#$"../TurnGauge".set_turn_down(2)
+	
+	# Round turn
+	await get_tree().create_timer(2.5).timeout
+	$"../RoundTurnGauge".set_turn_up(1)
+	await get_tree().create_timer(0.5).timeout
+	$"../RoundTurnGauge".set_turn_up(1)
+
+
+func end_monster_turn():
+	# End monster turn
+	if $"../PlayerHand".player_hand.size() > 6:
+		pass
+	else:
+		$"../Deck2".draw_card()
+	$"../Mana".reset_mana()
+	await get_tree().create_timer(0.5).timeout
+	if $"../PlayerHand".player_hand.size() > 6:
+		pass
+	else:
+		$"../Deck2".draw_card()
+	await get_tree().create_timer(1).timeout
+	if monster_buff_25 and $"../PlayerHand".player_hand.size() > 0:
+		$"../PlayerHand".player_hand[int(randi_range(0, $"../PlayerHand".player_hand.size()-1))].visible = false
+	$"../EndTurnButton".disabled = false
+	$"../EndTurnButton".visible = true
+	Global.monster_turn = false
+
+
+#func _on_button_pressed() -> void:
+	#get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- scripts2/DeckTuto.gd
+++ scripts2/DeckTuto.gd
@@ -1,1 +1,64 @@
 extends Node2D
+
+const CARD_SCENE_PATH = "res://scenes2/Card.tscn"
+const CARD_DRAW_SPEED = 1
+const STARTING_HAND_SIZE = Global.starting_hand_size
+
+
+var player_deck = [
+	"Eolapino","Eolapino", 
+	"Nuclearo", "Nuclearo", 
+	"Barrachat", "Barrachat", 
+	"Thermhibou", "Thermhibou", 
+	"Panolero", "Panolero", 
+	"Amigo Tri", 
+	"Nuclearo Destructo", 
+	"Taxito Gratuito", 
+	"Climato Froido",
+	"Foresto Anouvo"
+	]
+var card_database_reference
+var draw_card_this_turn = false
+
+
+func _ready() -> void:
+	player_deck.shuffle()
+	$Label.text = str(player_deck.size())
+	card_database_reference = preload("res://scripts2/CardDatabase.gd")
+	for i in range(STARTING_HAND_SIZE):
+		draw_card()
+		draw_card_this_turn = false
+	draw_card_this_turn = true
+
+
+func draw_card():
+	#if draw_card_this_turn:
+		#return
+	
+	draw_card_this_turn = true
+	
+	
+	# If player drew the last card in the deck, disable the deck
+	if player_deck.size() == 0:
+		$Area2D/CollisionShape2D.disabled = true
+		$Sprite2D.visible = false
+		$Label.visible = false
+		return
+	
+	var card_drawn_name = player_deck[0]
+	player_deck.erase(card_drawn_name)
+	$Label.text = str(player_deck.size())
+	var card_scene = preload(CARD_SCENE_PATH)
+	var new_card = card_scene.instantiate()
+	var card_image_path = str("res://assets/cards/" + card_drawn_name + ".png")
+	new_card.get_node("CardImage").texture = load(card_image_path)
+	new_card.get_node("ManaCost").text = str(card_database_reference.CARDS[card_drawn_name][0])
+	new_card.get_node("SocialCost").text = str(card_database_reference.CARDS[card_drawn_name][1])
+	new_card.get_node("EnvCost").text = str(card_database_reference.CARDS[card_drawn_name][2])
+	new_card.get_node("Damage").text = str(card_database_reference.CARDS[card_drawn_name][3])
+	#new_card.get_node("Description").text = card_database_reference.CARDS[card_drawn_name][4]
+	new_card.get_node("NameCard").text = card_drawn_name
+	$"../CardManager2".add_child(new_card)
+	new_card.name = "Card"
+	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
+	new_card.get_node("AnimationPlayer").play("card_flip")
